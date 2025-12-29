b212ed3b72b73d207310fc0fc0039d2149c27e75
Ashwin Phadke <23502062+ashwin-phadke@users.noreply.github.com>
2025-12-29 21:43:46 +0530

    updated messaging
    Signed-off-by: Ashwin Phadke <23502062+ashwin-phadke@users.noreply.github.com>

--- src/components/ContactTab.vue
+++ src/components/ContactTab.vue
@@ -7,8 +7,7 @@ defineProps<{
 <template>
     <div class="max-w-lg animate-in fade-in slide-in-from-bottom-4 duration-700">
         <h2 class="text-4xl font-bold mb-6">Let's create something together.</h2>
-        <p class="text-xl text-neutral-500 mb-12">I’m currently taking on new projects for 2024. Reach
-            out and say hello.
+        <p class="text-xl text-neutral-500 mb-12">
         </p>
         <a :href="`mailto:${email}`"
             class="text-3xl md:text-4xl font-bold border-b-4 border-neutral-200 hover:border-neutral-900 dark:hover:border-white transition-all pb-2 inline-block">
